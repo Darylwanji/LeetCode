21f4467f77f8e388d2433854c1b57c92eaaafcf7
Daryl Wanji <65738980+Darylwanji@users.noreply.github.com>
2023-05-16 20:29:56 -0400

    Time: 53 ms (56.33%), Space: 17.2 MB (22.97%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -2,7 +2,6 @@ import re
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         s = ''.join(char for char in s if char.isalnum()).lower()
-        print(s)
         if len(s)%2 == 0:
             return s[:len(s)//2] == (s[(len(s)//2):][::-1])
         else: 
